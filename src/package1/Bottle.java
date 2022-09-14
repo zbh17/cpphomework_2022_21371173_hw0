d5c6d5e9d794cb138797b98d7c471e944d8e3a85
ZhangBohao <3487056730@buaa.edu.cn>
2022-09-14 09:51:09 +0800

    Init commit

---
+++ src/package1/Bottle.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Bottle {
+
+    public static void main (String argu[]){
+        int a;
+        int b;
+        Scanner System.in.read(a);
+        System.in.read(b);
+        System.out.println(a+b);
+    }
+
+
+}
